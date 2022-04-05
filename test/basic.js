633024cf9f5ca5970a8054dbcb7888ae20062b2a
Gar <gar+gh@danger.computer>
2022-04-05 08:12:22 -0700

    deps: @npmcli/template-oss@3.2.2
    BREAKING CHANGE: this drops support for node 10 and non-LTS versions of
    node 12 and node 14

--- test/basic.js
+++ test/basic.js
@@ -1,25 +1,26 @@
 const t = require('tap')
 const mutateFS = require('mutate-fs')
 const inferOwner = require('../')
+const path = require('path')
 
 const owner = { uid: 420, gid: 69 }
 t.teardown(mutateFS.statMutate((er, st) => ([er, owner])))
 
 t.test('infer ownership', t => {
-  t.same(inferOwner.sync(__dirname + '/../a/b/sync'), owner)
-  return inferOwner(__dirname + '/x/y/async').then(res => t.same(res, owner))
+  t.same(inferOwner.sync(path.join(__dirname, '/../a/b/sync')), owner)
+  return inferOwner(path.join(__dirname, '/x/y/async')).then(res => t.same(res, owner))
 })
 
 t.test('pull from caches by running again', t => {
   // this doesn't have a different result, but coverage shows it worked
   // and that the caches are shared between sync and async
-  t.same(inferOwner.sync(__dirname + '/x/y'), owner)
-  return inferOwner(__dirname + '/../a/b').then(res => t.same(res, owner))
+  t.same(inferOwner.sync(path.join(__dirname, '/x/y')), owner)
+  return inferOwner(path.join(__dirname, '/../a/b')).then(res => t.same(res, owner))
 })
 
 t.test('inflight detection', t => Promise.all([
-  inferOwner(__dirname + '/in/flight'),
-  inferOwner(__dirname + '/x/../in/flight'),
+  inferOwner(path.join(__dirname, '/in/flight')),
+  inferOwner(path.join(__dirname, '/x/../in/flight')),
 ]).then(results => t.same(results[0], results[1])))
 
 t.test('all the way to the root', t => {
